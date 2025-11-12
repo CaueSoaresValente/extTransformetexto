32bef9ff129b31e9c44189fb19234f49b8dccdf7
Cauê Valente <cauevalente00@gmail.com>
2025-11-12 10:04:10 -0300

    Botões: Primeira letra maiúscula e a cada . uma letra maiúscula

--- script.js
+++ script.js
@@ -11,6 +11,22 @@ window.document.addEventListener("DOMContentLoaded", ()=>{
       let resultado = document.getElementById("resultado")
       resultado.innerHTML = ` Resultado: ${input}`
     })
+
+     document.getElementById('PrimeiraMaiuscula').addEventListener("click", ()=>{
+      let input = document.getElementById("input-text").value
+      input = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+      let resultado = document.getElementById("resultado")
+      resultado.innerHTML = ` Resultado: ${input}`
+    })
+    document.getElementById('SepararporPonto').addEventListener("click", ()=>{
+      let input = document.getElementById("input-text").value
+      input = input.replace(/\.\s*([a-z])/g, (letra) => {
+                              return letra.toUpperCase();
+                            })
+      let resultado = document.getElementById("resultado")
+      resultado.innerHTML = ` Resultado: ${input}`
+    })
+
     
 })
 
