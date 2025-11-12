a938e001e03281c94929a412def33749b3dde063
Cauê Valente <cauevalente00@gmail.com>
2025-11-12 08:44:26 -0300

    Atualizações no Json

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ window.document.addEventListener("DOMContentLoaded", ()=>{
     document.getElementById('btnMaiuscula').addEventListener("click", ()=>{
       let input = document.getElementById("input-text").value.toUpperCase()
       let resultado = document.getElementById("resultado")
-      resultado.innerHTML = `RESULTADO: ${input}`
+      resultado.innerHTML = ` Resultado: ${input}`
     })
 
     document.getElementById('btnMinuscula').addEventListener("click", ()=>{
       let input = document.getElementById("input-text").value.toLowerCase()
       let resultado = document.getElementById("resultado")
-      resultado.innerHTML = `RESULTADO: ${input}`
+      resultado.innerHTML = ` Resultado: ${input}`
     })
     
 })
+
