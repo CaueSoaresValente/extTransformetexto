5a38061bc790f08ca89a51382c9f3aaa6baf1c2a
Cauê Valente <cauevalente00@gmail.com>
2025-11-12 10:56:59 -0300

    Botões unificados e novo botão de limpar

--- script.js
+++ script.js
@@ -14,19 +14,18 @@ window.document.addEventListener("DOMContentLoaded", ()=>{
 
      document.getElementById('PrimeiraMaiuscula').addEventListener("click", ()=>{
       let input = document.getElementById("input-text").value
-      input = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
-      let resultado = document.getElementById("resultado")
-      resultado.innerHTML = ` Resultado: ${input}`
-    })
-    document.getElementById('SepararporPonto').addEventListener("click", ()=>{
-      let input = document.getElementById("input-text").value
-      input = input.replace(/\.\s*([a-z])/g, (letra) => {
+      input = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase().replace(/\.\s*([a-z])/g, (letra) => {
                               return letra.toUpperCase();
-                            })
+                            });
       let resultado = document.getElementById("resultado")
       resultado.innerHTML = ` Resultado: ${input}`
     })
 
+     document.getElementById('Limpar').addEventListener("click", ()=>{
+      document.getElementById("input-text").value = " "
+  
+      document.getElementById("resultado").innerHTML = " Resultado:"
+    }) 
     
 })
 
