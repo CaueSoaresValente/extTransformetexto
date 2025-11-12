f58f0094a352a78a92a07560d12ad7285a2de116
Cauê Valente <cauevalente00@gmail.com>
2025-11-12 11:29:29 -0300

    Nova funcionalidade (selecionar cor da resposta)

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ window.document.addEventListener("DOMContentLoaded", ()=>{
 
     document.getElementById('btnMaiuscula').addEventListener("click", ()=>{
       let input = document.getElementById("input-text").value.toUpperCase()
+      cor = document.getElementById("cor").value 
       let resultado = document.getElementById("resultado")
       resultado.innerHTML = ` Resultado: ${input}`
+      resultado.style.color = cor
     })
 
     document.getElementById('btnMinuscula').addEventListener("click", ()=>{
       let input = document.getElementById("input-text").value.toLowerCase()
+      cor = document.getElementById("cor").value 
       let resultado = document.getElementById("resultado")
       resultado.innerHTML = ` Resultado: ${input}`
+      resultado.style.color = cor
     })
 
      document.getElementById('PrimeiraMaiuscula').addEventListener("click", ()=>{
@@ -17,15 +21,18 @@ window.document.addEventListener("DOMContentLoaded", ()=>{
       input = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase().replace(/\.\s*([a-z])/g, (letra) => {
                               return letra.toUpperCase();
                             });
+      cor = document.getElementById("cor").value 
       let resultado = document.getElementById("resultado")
       resultado.innerHTML = ` Resultado: ${input}`
+      resultado.style.color = cor
     })
 
-     document.getElementById('Limpar').addEventListener("click", ()=>{
+    document.getElementById('Limpar').addEventListener("click", ()=>{
+      
       document.getElementById("input-text").value = " "
-  
       document.getElementById("resultado").innerHTML = " Resultado:"
-    }) 
     
+    }) 
 })
+
 
